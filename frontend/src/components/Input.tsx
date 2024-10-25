24398ebd9862c49dd876a50808cf08ae9c54d4bd
mastewal degu <mastewaldegu0@gmail.com>
2024-10-25 07:36:13 -0700

    update it

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -70,7 +70,7 @@ function Input({ socket }: { socket: React.MutableRefObject<Socket> }) {
             image,
         }
     
-        const res = await fetch(`https://hawky.onrender.com/api/message`, {
+        const res = await fetch(`http://localhost:5000/api/message`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -144,7 +144,7 @@ function Input({ socket }: { socket: React.MutableRefObject<Socket> }) {
                     image: ""
                 };
         
-                const res = await fetch(`https://hawky.onrender.com/api/message`, {
+                const res = await fetch(`http://localhost:5000/api/message`, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -30,7 +30,7 @@ function Login() {
         setLoading(true);
 
         
-        const res = await fetch(`https://hawky.onrender.com/api/user/login`, {
+        const res = await fetch(`http://localhost:5000/api/user/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
